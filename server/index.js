da859deadc167c354a595815a1732f8fa4d90409
shedrachIkenna <shedrach686@gmail.com>
2022-12-21 08:48:46 +0100

    Finished Login Auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { authRoutes } from './routes/auth.js'
 import mongoose from 'mongoose'
 import { register } from './controllers/auth.js'
 
@@ -43,6 +44,8 @@ const upload = multer({ storage });
 // Routes with files
 app.post('/auth/register', upload.single('picture'), register)
 
+//Routes
+app.use('/auth', authRoutes);
 
 //  Mongoose Setup
 const PORT = process.env.PORT || 6001;
