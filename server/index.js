62d061dc2f7ef119044cc97a99f78434c1ad2f17
shedrachIkenna <shedrach686@gmail.com>
2023-01-06 21:06:26 +0100

    added user.js logic in routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { authRoutes } from './routes/auth.js'
+import { userRoutes } from './routes/users.js'
 import mongoose from 'mongoose'
 import { register } from './controllers/auth.js'
 import { verifyToken } from './middleware/auth.js'
@@ -47,6 +48,7 @@ app.post('/auth/register', upload.single('picture'), register)
 
 //Routes
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes)
 
 //  Mongoose Setup
 const PORT = process.env.PORT || 6001;
