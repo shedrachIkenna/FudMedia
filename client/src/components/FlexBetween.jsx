9454f78e0bdb67177260aa27097edb93fbda04d5
shedrachIkenna <shedrach686@gmail.com>
2023-01-12 01:35:23 +0100

    Added reuseable styled component

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+import { styled } from '@mui/system'
+
+const  FlexBetween = styled()({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+})
+
+export default FlexBetween;
