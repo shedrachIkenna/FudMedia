745da076bb784d31896b247bc5d0a60d75edc8bb
shedrachIkenna <shedrach686@gmail.com>
2023-01-21 00:17:42 +0100

    Navbar light and dark mode completed

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material'
 import { styled } from '@mui/system'
 
-const  FlexBetween = styled()({
+const  FlexBetween = styled(Box)({
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
